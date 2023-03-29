65bbc0c60ce075d9751ff02db0707b9662053d64
Meonako <76484203+Meonako@users.noreply.github.com>
2023-03-29 20:41:36 +0700

    Remove: All comments

--- src/routes/view/[id]/lazyload.ts
+++ src/routes/view/[id]/lazyload.ts
@@ -6,26 +6,23 @@ let options = {
 
 export const lazyLoad = (image: HTMLImageElement, src: string) => {
 	const loaded = () => {
-		//image.classList.add('visible')                          // doesn't work in REPL
-		image.style.opacity = '1'; // REPL hack to apply loading animation
+		image.style.opacity = '1';
 	};
 	const observer = new IntersectionObserver((entries) => {
 		if (entries[0].isIntersecting) {
-			console.log('an image has loaded'); // console log for REPL
-			image.src = src; // replace placeholder src with the image src on observe
+			image.src = src;
 			if (image.complete) {
-				// check if instantly loaded
 				loaded();
 			} else {
-				image.addEventListener('load', loaded); // if the image isn't loaded yet, add an event listener
+				image.addEventListener('load', loaded);
 			}
 		}
 	}, options);
-	observer.observe(image); // intersection observer
+	observer.observe(image);
 
 	return {
 		destroy() {
-			image.removeEventListener('load', loaded); // clean up the event listener
+			image.removeEventListener('load', loaded);
 		}
 	};
 };
