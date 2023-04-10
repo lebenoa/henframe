966f68e9e4b3b8e97401205642fca0539b567289
Meonako <76484203+Meonako@users.noreply.github.com>
2023-04-10 09:33:31 +0700

    Change: `view` is now `CSR` only
    Change: `view` is now `CSR` only (since `SSR` broke lazyload)
    
    Move: fetch API endpoint to its own function

--- src/lib/nhql/api.ts
+++ src/lib/nhql/api.ts
@@ -1,1 +1,42 @@
+import type { APIByResponse } from './type';
+
 export const API_ENDPOINT = 'https://api.hifumin.app/v1/graphql';
+
+export async function getByID(id: number): Promise<APIByResponse> {
+	let requestBody = {
+		operationName: 'getByID',
+		variables: { id },
+		query: `
+			query getByID($id: Int!) {
+				nhql {
+					by(id: $id) {
+						success
+						data {
+							title {
+								display
+							}
+							images {
+								pages {
+									link
+									info {
+										width
+										height
+									}
+								}
+							}
+						}
+					} 
+				}
+			}`
+	};
+
+	let result = await fetch(API_ENDPOINT, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestBody)
+	});
+
+	return result.json();
+}
--- src/routes/view/[id]/+page.ts
+++ src/routes/view/[id]/+page.ts
@@ -1,53 +1,15 @@
-export const ssr = true;
-export const csr = false;
+export const ssr = false;
+export const csr = true;
 
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import { API_ENDPOINT } from '$lib/nhql/api';
-import type { APIByResponse } from '$lib/nhql/type';
+import { getByID } from '$lib/nhql/api';
 
 export const load = (async ({ params, fetch }) => {
 	if (!params.id) throw error(404, 'Not found');
+	else if (params.id.length == 6 && !/^\d+$/.test(params.id)) throw error(404, 'Not found');
 
-	if (!(params.id.length != 6) && !/^\d+$/.test(params.id)) {
-		throw error(404, 'Not found');
-	}
-
-	let requestBody = {
-		operationName: null,
-		variables: {},
-		query: `{
-                nhql {
-                    by(id: ${params.id}) {
-                        success
-                        data {
-							title {
-								display
-							}
-                            images {
-                                pages {
-                                    link
-									info {
-										width
-										height
-									}
-                                }
-                            }
-                        }
-                    } 
-                }
-            }`
-	};
-
-	let result = await fetch(API_ENDPOINT, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(requestBody)
-	});
-
-	let jsonData: APIByResponse = await result.json();
+	let jsonData = await getByID(parseInt(params.id));
 
 	return {
 		result: jsonData.data.nhql.by
