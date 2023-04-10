598d14bce7de9a83e2344c66c35827c41ee0f82a
Meonako <76484203+Meonako@users.noreply.github.com>
2023-04-10 11:54:33 +0700

    FMT: Prettier

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,10 @@
 // @ts-nocheck
 
-import { redirect } from "@sveltejs/kit";
+import { redirect } from '@sveltejs/kit';
 
 export const actions = {
 	search: async ({ request }) => {
 		const data = await request.formData();
-        throw redirect(308, `/search/${data.get('term')}/1`)
+		throw redirect(308, `/search/${data.get('term')}/1`);
 	}
 };
