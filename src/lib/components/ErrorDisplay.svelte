713449e3cee1d0da33279548c21c2fb3e8eb4668
Meonako <dubi.db28@gmail.com>
2024-07-08 16:19:43 +0700

    Remove: Unnecessary var rename

--- src/lib/components/ErrorDisplay.svelte
+++ src/lib/components/ErrorDisplay.svelte
@@ -1,8 +1,8 @@
 <script>
-    let { error: ERROR } = $props();
+    let { error } = $props();
 </script>
 
 <article class="prose prose-pink w-full h-full mx-auto text-center">
     <h1>Something went wrong</h1>
-    <h3>{ERROR}</h3>
+    <h3>{error}</h3>
 </article>
